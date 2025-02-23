7b57b58c3f796d1197278c0c265b19e5c1629957
Mohammad Imran <108978570+MrMi18@users.noreply.github.com>
2025-02-23 22:46:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+const str = prompt("enter str");
+
+
+const myMap = new Map();
+
+for(let i = 0 ; i<str.length;i++){
+if(myMap.has(str[i])){
+	myMap.set(str[i], [...(myMap.get(str[i])),i]);
+ }else{
+	myMap.set(str[i], [i]);
+ }
+}
+const obj = Object.fromEntries(myMap);
+console.log(obj);
+
